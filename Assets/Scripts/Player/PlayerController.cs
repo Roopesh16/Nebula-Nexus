1f89d817199bcd727e847ffcfbe6d125b109f79e
Roopesh Verma <roopeshv.30@gmail.com>
2024-05-14 16:31:58 +0530

    Add player controller and player move method

---
+++ Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+namespace NebulaNexus.Player
+{
+    public class PlayerController
+    {
+        private PlayerView playerView;
+        private PlayerScriptableObject playerSO;
+
+        public PlayerController(PlayerView playerView, PlayerScriptableObject playerSO)
+        {
+            this.playerView = playerView;
+            this.playerSO = playerSO;
+            playerView.SetController(this);
+        }
+
+        public void MovePlayer(Direction direction)
+        {
+            if (direction == Direction.LEFT)
+                playerView.transform.Translate(-playerView.transform.right * playerSO.moveSpeed * Time.deltaTime);
+            else
+                playerView.transform.Translate(playerView.transform.right * playerSO.moveSpeed * Time.deltaTime);
+        }
+    }
+}
