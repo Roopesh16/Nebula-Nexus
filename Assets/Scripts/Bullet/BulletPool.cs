dd468d5358890e50eedf3b7524087d5054ce6337
Roopesh Verma <roopeshv.30@gmail.com>
2024-05-14 17:25:19 +0530

    Add Bullet Pool

---
+++ Assets/Scripts/Bullet/BulletPool.cs
@@ -0,0 +1,21 @@
+﻿using NebulaNexus.Utilities;
+
+namespace NebulaNexus.Bullet
+{
+    public class BulletPool : GenericObjectPool<BulletController>
+    {
+        private BulletView bulletPrefab;
+        private BulletScriptableObject bulletSO;
+
+        public BulletPool(BulletView bulletPrefab, BulletScriptableObject bulletSO)
+        {
+            this.bulletPrefab = bulletPrefab;
+            this.bulletSO = bulletSO;
+        }
+
+        protected override BulletController CreateItem()
+        {
+            return new BulletController(bulletPrefab, bulletSO);
+        }
+    }
+}
