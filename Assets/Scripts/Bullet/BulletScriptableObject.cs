e7375ac6f55c2f57ee27b8a20397d54ec07c6e30
Roopesh Verma <roopeshv.30@gmail.com>
2024-05-14 17:16:27 +0530

    Add bullet so script

---
+++ Assets/Scripts/Bullet/BulletScriptableObject.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+namespace NebulaNexus.Bullet
+{
+    [CreateAssetMenu(fileName = "Bullet SO", menuName = "ScriptableObject/Bullet")]
+    public class BulletScriptableObject : ScriptableObject
+    {
+        public float moveSpeed = 6f;
+        public Sprite bulletSprite;
+    }
+}
