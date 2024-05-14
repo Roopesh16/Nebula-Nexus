4ec2069e09753e2e026dc29a405f2e0d4e85f514
Roopesh Verma <roopeshv.30@gmail.com>
2024-05-14 19:14:55 +0530

    Add bullet controller

---
+++ Assets/Scripts/Bullet/BulletController.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using NebulaNexus.Interfaces;
+
+namespace NebulaNexus.Bullet
+{
+    public class BulletController : IBullet
+    {
+        private BulletView bulletView;
+        private BulletScriptableObject bulletSO;
+
+        public BulletController(BulletView bulletPrefab, BulletScriptableObject bulletSO)
+        {
+            bulletView = Object.Instantiate(bulletPrefab);
+            bulletView.SetController(this);
+            this.bulletSO = bulletSO;
+        }
+
+        public void ConfigureBullet(Transform spawnPosition)
+        {
+            bulletView.gameObject.SetActive(true);
+            bulletView.transform.localPosition = spawnPosition.position;
+            bulletView.transform.localRotation = spawnPosition.rotation;
+        }
+
+        public void MoveBullet()
+        {
+            bulletView.transform.Translate(bulletView.transform.up * bulletSO.moveSpeed * Time.deltaTime);
+        }
+
+        public void OnTrigger(GameObject other)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
