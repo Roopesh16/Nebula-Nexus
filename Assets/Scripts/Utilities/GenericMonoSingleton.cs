5ccaf78e320d0d40782cebe5f8d6ca4df3a57712
Roopesh Verma <roopeshv.30@gmail.com>
2024-05-14 13:29:53 +0530

    Add generic mono singleton

---
+++ Assets/Scripts/Utilities/GenericMonoSingleton.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class GenericMonoSingleton<T> : MonoBehaviour where T : GenericMonoSingleton<T>
+{
+    private static T instance = null;
+
+    public static T Instance { get { return instance; } }
+
+    /// <summary>
+    /// Generic Base Awake Call
+    /// </summary>
+    protected virtual void Awake()
+    {
+        if (instance == null)
+            instance = (T)this;
+        else if (instance != this)
+            Destroy(gameObject);
+    }
+}
