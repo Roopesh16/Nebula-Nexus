06ffe572ad4ab45a87ab51a977ffcb1a1c6b6761
Roopesh Verma <roopeshv.30@gmail.com>
2024-05-14 23:34:04 +0530

    Add enemy controller

---
+++ Assets/Scripts/Enemy/EnemyController.cs
@@ -0,0 +1,32 @@
+﻿using NebulaNexus.Bullet;
+using NebulaNexus.Main;
+using UnityEngine;
+
+namespace NebulaNexus.Enemy
+{
+    public class EnemyController
+    {
+        private EnemyView enemyView;
+        private EnemyScriptableObject enemySO;
+        private float rateOfFire = 0f;
+
+        public EnemyController(EnemyView enemyView, EnemyScriptableObject enemySO)
+        {
+            this.enemyView = enemyView;
+            this.enemyView.SetController(this);
+            this.enemySO = enemySO;
+        }
+
+        public void ShootBullet()
+        {
+            if (rateOfFire < enemySO.RateOfFire)
+                rateOfFire += Time.deltaTime;
+            else
+            {
+                BulletController bullet = GameService.Instance.EnemyService.GetBullet();
+                bullet.ConfigureBullet(enemyView.SpawnPosition);
+                rateOfFire = 0f;
+            }
+        }
+    }
+}
