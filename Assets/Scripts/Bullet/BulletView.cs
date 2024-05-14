e849dd24e5eb6fbb9b135081f6b5e6d681248e9c
Roopesh Verma <roopeshv.30@gmail.com>
2024-05-14 17:16:18 +0530

    Add Bullet view

---
+++ Assets/Scripts/Bullet/BulletView.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+namespace NebulaNexus.Bullet
+{
+    public class BulletView : MonoBehaviour
+    {
+        private BulletController bulletController;
+
+        public void SetController(BulletController bulletController) => this.bulletController = bulletController;
+
+        private void Update() => bulletController?.MoveBullet();
+
+        private void OnTriggerEnter2D(Collider2D other) => bulletController.OnTrigger(other.gameObject);
+    }
+}
