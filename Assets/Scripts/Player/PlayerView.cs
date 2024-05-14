cc5c5e85c7379077718ec4ab42466d33cd8b471f
Roopesh Verma <roopeshv.30@gmail.com>
2024-05-14 20:17:51 +0530

    Add Shoot input method

--- Assets/Scripts/Player/PlayerView.cs
+++ Assets/Scripts/Player/PlayerView.cs
@@ -4,16 +4,30 @@ namespace NebulaNexus.Player
 {
     public class PlayerView : MonoBehaviour
     {
+        [SerializeField] private Transform spawnPosition;
         private PlayerController playerController;
 
         public void SetController(PlayerController playerController) => this.playerController = playerController;
 
         private void Update()
         {
+            MovementInput();
+            ShootInput();
+        }
+
+        private void MovementInput()
+        {
             if (Input.GetAxisRaw("Horizontal") > 0)
                 playerController.MovePlayer(Direction.RIGHT);
             if (Input.GetAxisRaw("Horizontal") < 0)
                 playerController.MovePlayer(Direction.LEFT);
+        }
+
+        private void ShootInput()
+        {
+            if (Input.GetMouseButton(0))
+                playerController.ShootBullet(spawnPosition);
+
         }
     }
 }
