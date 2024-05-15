96ba39b27e826bd21b7850f08878bc584726609e
Roopesh Verma <roopeshv.30@gmail.com>
2024-05-15 14:03:46 +0530

    Remove bullet prefab

--- Assets/Scripts/Enemy/EnemyService.cs
+++ Assets/Scripts/Enemy/EnemyService.cs
@@ -6,18 +6,15 @@ namespace NebulaNexus.Enemy
     public class EnemyService
     {
         private EnemyController enemyController;
-        private BulletView bulletPrefab;
         private BulletScriptableObject bulletSO;
         private Transform bulletParent;
 
-        public BulletView BulletPrefab => bulletPrefab;
         public BulletScriptableObject BulletSO => bulletSO;
         public Transform BulletParent => bulletParent;
 
         public EnemyService(EnemyView enemyView, EnemyScriptableObject enemyScriptableObject,
-                            BulletScriptableObject bulletSO, BulletView bulletPrefab, Transform bulletParent)
+                            BulletScriptableObject bulletSO, Transform bulletParent)
         {
-            this.bulletPrefab = bulletPrefab;
             this.bulletSO = bulletSO;
             this.bulletParent = bulletParent;
             EnemyController enemyController = new EnemyController(enemyView, enemyScriptableObject);
