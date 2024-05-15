c8407a91e86c318bac4df676bb42f718a7d80f4d
Roopesh Verma <roopeshv.30@gmail.com>
2024-05-15 14:12:23 +0530

    Add subtype to T

--- Assets/Scripts/Utilities/GenericObjectPool.cs
+++ Assets/Scripts/Utilities/GenericObjectPool.cs
@@ -24,28 +24,28 @@ namespace NebulaNexus.Utilities
         /// Method to get item from pooled list
         /// </summary>
         /// <returns>Generic Pooled Type</returns>
-        public T GetItem()
+        public T GetItem<U>() where U : T
         {
             if (pooledList.Count > 0)
             {
-                PooledItem<T> pooledItem = pooledList.Find(item => !item.IsUsed);
+                PooledItem<T> pooledItem = pooledList.Find(item => !item.IsUsed && item.Item is U);
                 if (pooledItem != null)
                 {
                     pooledItem.IsUsed = true;
                     return pooledItem.Item;
                 }
             }
-            return CreateNewItem();
+            return CreateNewItem<U>();
         }
 
         /// <summary>
         /// If not available, create new pooled items
         /// </summary>
         /// <returns>Generic Pooled Type</returns>
-        private T CreateNewItem()
+        private T CreateNewItem<U>()
         {
             PooledItem<T> pooledItem = new();
-            pooledItem.Item = CreateItem();
+            pooledItem.Item = CreateItem<U>();
             pooledItem.IsUsed = true;
             pooledList.Add(pooledItem);
             return pooledItem.Item;
@@ -55,15 +55,15 @@ namespace NebulaNexus.Utilities
         /// To be overriden by child class, to create pooled item object
         /// </summary>
         /// <returns>Generic Pooled Type</returns>
-        protected abstract T CreateItem();
+        protected abstract T CreateItem<U>();
 
         /// <summary>
         /// Return the object to the list
         /// </summary>
         /// <param name="item">Generic Object Type</param>
-        public void ReturnItem(T item)
+        public void ReturnItem<U>(T item)
         {
-            PooledItem<T> pooledItem = pooledList.Find(i => i.Item.Equals(item));
+            PooledItem<T> pooledItem = pooledList.Find(i => i.Item.Equals(item) && i.Item is U);
             pooledItem.IsUsed = false;
         }
     }
