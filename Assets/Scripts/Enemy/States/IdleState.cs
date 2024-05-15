950444e27852831cabcc37cad1eb4bdd31b8d3ec
Roopesh Verma <roopeshv.30@gmail.com>
2024-05-15 21:28:46 +0530

    Add Idle state

---
+++ Assets/Scripts/Enemy/States/IdleState.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using NebulaNexus.Enemy.StateMachine;
+
+namespace NebulaNexus.Enemy.States
+{
+    public class IdleState : IState
+    {
+        public EnemyController Owner { get; set; }
+
+        public void OnStateEnter()
+        {
+            Owner.Enemy.transform.position = Owner.Data.StartPostion;
+        }
+
+        public void Update()
+        {
+            if (Vector2.Distance(Owner.Enemy.transform.position, Owner.Data.EndPosition) > Owner.Data.MinDistance)
+            {
+                Owner.Enemy.transform.Translate(Owner.Data.DownSpeed * Time.deltaTime * -Owner.Enemy.transform.up);
+            }
+            else
+            {
+                Owner.Enemy.transform.position = Owner.Data.EndPosition;
+                Owner.StateMachine.ChangeState(StateMachine.States.SHOOTING);
+            }
+        }
+
+        public void OnStateExit()
+        { }
+    }
+}
