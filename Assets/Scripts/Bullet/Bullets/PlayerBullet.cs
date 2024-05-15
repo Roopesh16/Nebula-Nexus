499456259256407443f513b25c125401f424085e
Roopesh Verma <roopeshv.30@gmail.com>
2024-05-15 13:42:15 +0530

    Add player bullet

---
+++ Assets/Scripts/Bullet/Bullets/PlayerBullet.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace NebulaNexus.Bullet
+{
+    public class PlayerBullet : BulletController
+    {
+        public PlayerBullet(BulletView bulletPrefab, BulletScriptableObject bulletSO, Transform parent) :
+            base(bulletPrefab, bulletSO, parent)
+        { }
+
+        public override void MoveBullet()
+        {
+            base.MoveBullet();
+        }
+
+        public override void OnTrigger(GameObject other)
+        {
+            base.OnTrigger(other);
+        }
+    }
+}
