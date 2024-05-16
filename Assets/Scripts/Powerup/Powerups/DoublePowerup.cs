8f0d3ed79befa6af242524049e8e5478e66e239c
Roopesh Verma <roopeshv.30@gmail.com>
2024-05-16 15:52:49 +0530

    Add Double powerup

---
+++ Assets/Scripts/Powerup/Powerups/DoublePowerup.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace NebulaNexus.Powerup
+{
+    public class DoublePowerup : PowerupController
+    {
+        public DoublePowerup(PowerupView powerupView, Transform parent, PowerupType powerupType, Sprite powerupSprite) :
+            base(powerupView, parent, powerupType, powerupSprite)
+        { }
+
+        public override void OnTrigger(GameObject other)
+        {
+            Debug.Log("Double");
+            base.OnTrigger(other);
+        }
+    }
+}
