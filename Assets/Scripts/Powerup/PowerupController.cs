bf687d36207e1434415a7ec11d90288162941ed7
Roopesh Verma <roopeshv.30@gmail.com>
2024-05-16 15:54:54 +0530

    Add powerup controller

---
+++ Assets/Scripts/Powerup/PowerupController.cs
@@ -0,0 +1,34 @@
+﻿using NebulaNexus.Interfaces;
+using NebulaNexus.Main;
+using System.Collections;
+using UnityEngine;
+
+namespace NebulaNexus.Powerup
+{
+    public class PowerupController : IPowerup
+    {
+        protected PowerupView powerupView;
+        protected PowerupType powerupType;
+
+        public PowerupType PowerupType => powerupType;
+
+        public PowerupController(PowerupView powerupView, Transform parent, PowerupType powerupType, Sprite powerupSprite)
+        {
+            this.powerupView = Object.Instantiate(powerupView, parent);
+            this.powerupView.SetController(this);
+            this.powerupType = powerupType;
+            this.powerupView.SetPowerupSprite(powerupSprite);
+        }
+
+        public virtual void OnTrigger(GameObject other)
+        {
+            ReturnToPool();
+        }
+
+        private void ReturnToPool()
+        {
+            powerupView.gameObject.SetActive(false);
+            GameService.Instance.PowerupService.ReturnPowerup(this);
+        }
+    }
+}
