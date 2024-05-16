7a15148e606384bc6ec32b0a200cde7e7dc948a3
Roopesh Verma <roopeshv.30@gmail.com>
2024-05-16 15:52:56 +0530

    Add Multiple powerup

---
+++ Assets/Scripts/Powerup/Powerups/MultiplePowerup.cs
@@ -0,0 +1,19 @@
+﻿using NebulaNexus.Powerup;
+using System.Collections;
+using UnityEngine;
+
+namespace NebulaNexus.Powerup
+{
+    public class MultiplePowerup : PowerupController
+    {
+        public MultiplePowerup(PowerupView powerupView, Transform parent, PowerupType powerupType, Sprite powerupSprite) :
+            base(powerupView, parent, powerupType, powerupSprite)
+        { }
+
+        public override void OnTrigger(GameObject other)
+        {
+            Debug.Log("Multiple");
+            base.OnTrigger(other);
+        }
+    }
+}
