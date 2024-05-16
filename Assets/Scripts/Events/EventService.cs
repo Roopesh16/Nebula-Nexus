48475898b27e3404eb474ba23c32555ceb57054f
Roopesh Verma <roopeshv.30@gmail.com>
2024-05-16 05:40:51 +0530

    Add enemy active event

--- Assets/Scripts/Events/EventService.cs
+++ Assets/Scripts/Events/EventService.cs
@@ -3,10 +3,11 @@
     public class EventService
     {
         public EventController OnPlayClick { get; private set; }
-
+        public EventController OnEnemyActive { get; private set; }
         public EventService()
         {
             OnPlayClick = new EventController();
+            OnEnemyActive = new EventController();
         }
     }
 }
