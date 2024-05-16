8a69b1cf94044bd5a5960bc208bb8617bdabc245
Roopesh Verma <roopeshv.30@gmail.com>
2024-05-16 17:30:05 +0530

    Start timer and move powerups down

--- Assets/Scripts/Powerup/PowerupView.cs
+++ Assets/Scripts/Powerup/PowerupView.cs
@@ -15,7 +15,11 @@ namespace NebulaNexus.Powerup
 
         public PowerupType GetPowerupType() => powerupController.PowerupSO.PowerupType;
 
-        private void Update() => powerupController?.MovePowerup();
+        private void Update()
+        {
+            powerupController?.StartTimer();
+            powerupController?.MovePowerup();
+        }
 
         private void OnTriggerEnter2D(Collider2D other) => powerupController?.OnTrigger(other.gameObject);
     }
