0248c8a46a75db57e72e6b6846eaea2a17e2623c
Roopesh Verma <roopeshv.30@gmail.com>
2024-05-16 15:55:05 +0530

    Add powerup pool

---
+++ Assets/Scripts/Powerup/PowerupPool.cs
@@ -0,0 +1,39 @@
+﻿using NebulaNexus.Utilities;
+using System;
+using UnityEngine;
+
+namespace NebulaNexus.Powerup
+{
+    public class PowerupPool : GenericObjectPool<PowerupController>
+    {
+        private PowerupView powerupPrefab;
+        private PowerupType powerupType;
+        private Sprite powerupSprite;
+        private Transform parent;
+
+        public PowerupPool(PowerupView powerupPrefab)
+        {
+            this.powerupPrefab = powerupPrefab;
+        }
+
+        public PowerupController GetPowerup<U>(PowerupType powerupType, Transform parent, Sprite powerupSprite) where U : PowerupController
+        {
+            this.powerupType = powerupType;
+            this.powerupSprite = powerupSprite;
+            this.parent = parent;
+            return GetItem<U>();
+        }
+
+        protected override PowerupController CreateItem<U>() where U : class
+        {
+            if (typeof(U) == typeof(DoublePowerup))
+                return new DoublePowerup(powerupPrefab, parent, powerupType, powerupSprite);
+            else if (typeof(U) == typeof(DoublePowerup))
+                return new MultiplePowerup(powerupPrefab, parent, powerupType, powerupSprite);
+
+            throw new NotSupportedException("Invalid Powerup type!");
+        }
+
+        public void ReturnPowerup(PowerupController returnPowerup) => ReturnItem(returnPowerup);
+    }
+}
