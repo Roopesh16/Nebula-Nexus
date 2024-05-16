6e7ed8ceeace65393474416d3b7a98fc3e8dec66
Roopesh Verma <roopeshv.30@gmail.com>
2024-05-16 15:52:40 +0530

    Add powerup service ref

--- Assets/Scripts/Main/GameService.cs
+++ Assets/Scripts/Main/GameService.cs
@@ -5,6 +5,9 @@ using NebulaNexus.Utilities;
 using NebulaNexus.Bullet;
 using NebulaNexus.Enemy;
 using NebulaNexus.Events;
+using NebulaNexus.Powerup;
+using System.Collections.Generic;
+using System;
 
 namespace NebulaNexus.Main
 {
@@ -28,12 +31,18 @@ namespace NebulaNexus.Main
         [Header("Bullet Service")]
         [SerializeField] private BulletView bulletPrefab;
 
+        [Header("Powerup Service")]
+        [SerializeField] private PowerupView powerupPrefab;
+        [SerializeField] private PowerupSprites powerupSprites;
+        [SerializeField] private List<Transform> spawnPostions = new();
+
 
         public EventService EventService { get; private set; }
         public GameManager GameManager { get; private set; }
         public PlayerService PlayerService { get; private set; }
         public EnemyService EnemyService { get; private set; }
         public BulletService BulletService { get; private set; }
+        public PowerupService PowerupService { get; private set; }
         public UIService UIService => uIService;
 
         /// <summary>
@@ -55,6 +64,14 @@ namespace NebulaNexus.Main
             PlayerService = new PlayerService(playerView, playerSO, bulletSO, bulletParent);
             EnemyService = new EnemyService(enemyView, enemyScriptableObject, enemyBulletSO, enemyBulletParent);
             BulletService = new BulletService(bulletPrefab);
+            PowerupService = new PowerupService(powerupPrefab, powerupSprites, spawnPostions);
         }
+    }
+
+    [Serializable]
+    public struct PowerupSprites
+    {
+        public Sprite DoubleSprite;
+        public Sprite MultipleSprite;
     }
 }
