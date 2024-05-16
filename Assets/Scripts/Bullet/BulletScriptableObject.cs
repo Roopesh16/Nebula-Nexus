378e8e56d43d1a84959d97e58e99419bb6f7f0de
Roopesh Verma <roopeshv.30@gmail.com>
2024-05-16 10:13:40 +0530

    Add health and layer data to SO

--- Assets/Scripts/Bullet/BulletScriptableObject.cs
+++ Assets/Scripts/Bullet/BulletScriptableObject.cs
@@ -5,7 +5,12 @@ namespace NebulaNexus.Bullet
     [CreateAssetMenu(fileName = "Bullet SO", menuName = "ScriptableObject/Bullet")]
     public class BulletScriptableObject : ScriptableObject
     {
-        public float moveSpeed = 6f;
-        public Sprite bulletSprite;
+        public float MoveSpeed = 6f;
+        public Sprite BulletSprite;
+        public LayerMask BulletLayer;
+        public string Tag;
+        public int MaxHealth = 100;
+        public int Damage;
+
     }
 }
