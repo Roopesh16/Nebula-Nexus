8e8a6237dbb8e655bcb2ce3ccca9f94f6e2e66f9
Roopesh Verma <roopeshv.30@gmail.com>
2024-05-16 17:10:39 +0530

    Add powerup SO

---
+++ Assets/Scripts/Powerup/PowerupScriptableObject.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace NebulaNexus.Powerup
+{
+    [CreateAssetMenu(fileName = "Powerup SO", menuName = "ScriptableObject/Powerups")]
+    public class PowerupScriptableObject : ScriptableObject
+    {
+        public PowerupType PowerupType;
+        public Sprite PowerupSprite;
+        public float MoveSpeed = 2f;
+    }
+}
